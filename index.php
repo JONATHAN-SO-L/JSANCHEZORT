b214dd6c84fa89b03a0fda5f541e97b52f2a6584
JONATHAN-SO-L <sanceznarval00@outlook.com>
2020-09-10 15:52:02 -0500

    Se incluye cv y se crea index para formulario de contacto

--- index.php
+++ index.php
@@ -41,8 +41,11 @@
       </a>
     </div>
     <div id="collapseOne" class="collapse show" data-parent="#accordion">
-      <div class="card-body">
-        <center>Lorem ipsum..</center>
+      <div class="card-body" id="card-body">
+        <center>
+        	<strong>PT-B en Soporte y Mantenimiento de Equipo de Cómputo</strong>
+        	<br>Egresado del Colegio Nacional de Educación Profesional Técnica (CONALEP) 241 Plantel Cuernavaca en busca de <i>oportunidad</i> dentro del área de <strong>Administración de Servidores</strong> y/o Desarrollo Web (DevOps).<br>
+        </center>
       </div>
     </div>
   </div>
@@ -55,7 +58,10 @@
     </div>
     <div id="collapseTwo" class="collapse" data-parent="#accordion">
       <div class="card-body">
-        <center>Lorem ipsum..</center>
+        <center>
+        	Dedico mi tiempo libre a <strong>aprender cosas nuevas</strong> relacionadas a las tecnologías de la información, buscando desarrollarme profesionalmente como Developer y SysAdmin (DevOps).
+        	<br>Sin dejar de lado el dedicar todo el tiempo necesario a mi familia y a disfrutar de la vida junto a su hermosa compañía
+        </center>
       </div>
     </div>
   </div>
@@ -68,7 +74,14 @@
     </div>
     <div id="collapseThree" class="collapse" data-parent="#accordion">
       <div class="card-body">
-        <center>Lorem ipsum..</center>
+        <center>
+        	<strong>CORTO PLAZO</strong>
+        	<br>Ser un Developer que proporcione soluciones e implemente tecnologías que faciliten tanto el uso como la experiencia de trabajo del usuario mediante aplicaciones web estructuradas y amigables para el uso rudimentario del día a día, adaptando las tecnologías a las necesidades de las empresas y clientes.<br><br>
+        	<strong>MEDIANO PLAZO</strong>
+        	<br>Ser un SysAdmin que proporcione soluciones e implemente infraestructuras tecnologicas que faciliten el uso y experiencia de trabajo del usuario reduciendo costos mediante el uso de tecnologías OpenSource y/o de Código Abierto adaptandolas a las necesidades de las empresas y clientes.<br><br>
+        	<strong>LARGO PLAZO</strong>
+        	<br>Ser un Developer Operative (DevOps) que dirija, proponga, coordine, disponga e implemente tecnologías e infraestructuras tecnologicas que faciliten el uso y la experiencia de trabajo del usuario reduciendo costos mediante tecnologías de diferentes fuentes junto con aplicaciones web estructuradas basadas en lenguajes de programación aptos para cada situación dependiendo de las necesidades de las empresas, lugares, bienes y clientes.<br>
+        </center>
       </div>
     </div>
   </div>
@@ -81,7 +94,32 @@
     </div>
     <div id="collapseFour" class="collapse" data-parent="#accordion">
       <div class="card-body">
-        <center>Lorem ipsum..</center>
+        <center>
+        	<strong>VIRTUDES</strong>
+        	<ul class="list-inline">
+        		<li>1) Dedicado y comprometido con el trabajo</li>
+        		<li>2) Responsable</li>
+        		<li>3) Capaz de trabajar de forma individual o en equipo</li>
+        		<li>4) Capaz de trabajar bajo estrés</li>
+        		<li>5) Paciente</li>
+        		<li>6) Proactivo</li>
+        		<li>7) Autodidacta</li>
+        		<li>8) De aprendizaje rápido</li>
+        		<li>9) Respetuoso</li>
+        		<li>10) Amigable</li>
+        		<li>11) Fomentador de un ambiente de trabajo agradable</li>
+        	</ul>
+        	<strong>DEFECTOS</strong>
+        	<ul class="list-inline">
+        		<li>1) Suelo flojear cuando el trabajo ya no me apasiona</li>
+        		<li>2) Tengo más compromiso con el trabajo que con la empresa</li>
+        		<li>3) No se me facilita realizar más de 5 actividades al mismo tiempo</li>
+        		<li>4) Susceptible al estrés después de mucho tiempo</li>
+        		<li>5) Suelo cumplir al pie de la letra los horarios, pocas veces los rebaso</li>
+        		<li>6) Me tomo el tiempo necesario para aprender, lo que implica un leve retraso en proyectos de los cuales yo no tenga conocimiento en las tecnologías que se aplican</li>
+        		<li>7) Suelo llegar tarde cuando existen proyectos a los que yo les de seguimiento fuera del trabajo</li>
+        	</ul>
+        </center>
       </div>
     </div>
   </div>
@@ -94,9 +132,9 @@
 	<div class="container" align="center">
 		<a class="boton" href="https://github.com/JONATHAN-SO-L" target="_blank"><button style="margin: 10px" type="sumbit" class="btn btn-success btn-lg"><img id="repos" src="https://www.pngkey.com/png/full/178-1787243_github-icon-png-github-icon-white-png.png">MIS REPOSITORIOS</button></a>
 
-		<a class="boton" href="#"><button style="margin: 10px" type="sumbit" class="btn btn-success btn-lg"><img id="cv" src="https://4.bp.blogspot.com/-MXOGcpIt2Ac/WlxKGTwZFnI/AAAAAAAAAKk/FLqTJuXVDBwWOxvVeur5SC1ofEi5CiRSgCLcBGAs/s320/hd-flat-face-icon-cdr.jpg">MI CURRÍCULUM</button></a>
+		<a class="boton" href="/jsanchezort/cv/Jonathán_Jaziel_Sánchez_Ortiz.pdf"><button style="margin: 10px" type="sumbit" class="btn btn-success btn-lg"><img id="cv" src="https://4.bp.blogspot.com/-MXOGcpIt2Ac/WlxKGTwZFnI/AAAAAAAAAKk/FLqTJuXVDBwWOxvVeur5SC1ofEi5CiRSgCLcBGAs/s320/hd-flat-face-icon-cdr.jpg">MI CURRÍCULUM</button></a>
 
-		<a class="boton" href="#"><button style="margin: 10px" type="sumbit" class="btn btn-success btn-lg"><img id="contact" src="https://lh3.googleusercontent.com/proxy/E_l7YvI4p7Q_x4OzHcaksaYG6nj5iJSGcKCLjurklXmriuYUCQEAbP3fX8MIeuiJkUKVPKGDqDtBqCdUpJJ5Iufp_FuvwqqJl3KrrK9Xo8D6F_l0Q8cN">CONTÁCTAME</button></a>
+		<a class="boton" href="/jsanchezort/config/"><button style="margin: 10px" type="sumbit" class="btn btn-success btn-lg"><img id="contact" src="https://lh3.googleusercontent.com/proxy/E_l7YvI4p7Q_x4OzHcaksaYG6nj5iJSGcKCLjurklXmriuYUCQEAbP3fX8MIeuiJkUKVPKGDqDtBqCdUpJJ5Iufp_FuvwqqJl3KrrK9Xo8D6F_l0Q8cN">CONTÁCTAME</button></a>
 	</div>
 
 </div>

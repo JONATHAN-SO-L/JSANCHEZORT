aead5e64f86f6cc4a55afefec744e59a8c79a1f1
JONATHAN-SO-L <sanceznarval00@outlook.com>
2020-09-10 02:19:44 -0500

    Se crea plantilla index y css

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <head>
 	<title>HOME</title>
 	<meta charset="utf-8">
+	<link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/6/63/Code_vmc2015.png" type="image/icon">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="/st/css/index.css">
+	<link rel="stylesheet" type="text/css" href="/jsanchezort/css/index.css">
 </head>
 
 <body>
@@ -16,7 +17,7 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 
   <a class="navbar-brand" href="#">
-    <img src="#" alt="logo" style="width:40px;">
+    <img src="https://pensotec.com/wp-content/uploads/2015/06/service.png" alt="logo" style="width:40px;">
   </a>
 
   <ul class="navbar-nav">
@@ -27,3 +28,89 @@
 	</li>
   </ul>
 </nav>
+
+<br>
+
+<div class="container">
+	<div id="accordion">
+
+  <div class="card">
+    <div class="card-header">
+      <a class="card-link acordeon" data-toggle="collapse" href="#collapseOne">
+        <center>¿QUIEN SOY?</center>
+      </a>
+    </div>
+    <div id="collapseOne" class="collapse show" data-parent="#accordion">
+      <div class="card-body">
+        <center>Lorem ipsum..</center>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <a class="collapsed card-link acordeon" data-toggle="collapse" href="#collapseTwo">
+        <center>¿AH QUÉ ME DEDICO EN MI TIEMPO LIBRE?</center>
+      </a>
+    </div>
+    <div id="collapseTwo" class="collapse" data-parent="#accordion">
+      <div class="card-body">
+        <center>Lorem ipsum..</center>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <a class="collapsed card-link acordeon" data-toggle="collapse" href="#collapseThree">
+        <center>¿CUÁLES SON MIS METAS LABORALES Y PROFESIONALES?</center>
+      </a>
+    </div>
+    <div id="collapseThree" class="collapse" data-parent="#accordion">
+      <div class="card-body">
+        <center>Lorem ipsum..</center>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <a class="collapsed card-link acordeon" data-toggle="collapse" href="#collapseFour">
+        <center>¿CUÁLES SON MIS VIRTUDES Y DEFECTOS?</center>
+      </a>
+    </div>
+    <div id="collapseFour" class="collapse" data-parent="#accordion">
+      <div class="card-body">
+        <center>Lorem ipsum..</center>
+      </div>
+    </div>
+  </div>
+
+</div>
+</div>
+
+<div class="botones">
+
+	<div class="container" align="center">
+		<a class="boton" href="https://github.com/JONATHAN-SO-L" target="_blank"><button style="margin: 10px" type="sumbit" class="btn btn-success btn-lg"><img id="repos" src="https://www.pngkey.com/png/full/178-1787243_github-icon-png-github-icon-white-png.png">MIS REPOSITORIOS</button></a>
+
+		<a class="boton" href="#"><button style="margin: 10px" type="sumbit" class="btn btn-success btn-lg"><img id="cv" src="https://4.bp.blogspot.com/-MXOGcpIt2Ac/WlxKGTwZFnI/AAAAAAAAAKk/FLqTJuXVDBwWOxvVeur5SC1ofEi5CiRSgCLcBGAs/s320/hd-flat-face-icon-cdr.jpg">MI CURRÍCULUM</button></a>
+
+		<a class="boton" href="#"><button style="margin: 10px" type="sumbit" class="btn btn-success btn-lg"><img id="contact" src="https://lh3.googleusercontent.com/proxy/E_l7YvI4p7Q_x4OzHcaksaYG6nj5iJSGcKCLjurklXmriuYUCQEAbP3fX8MIeuiJkUKVPKGDqDtBqCdUpJJ5Iufp_FuvwqqJl3KrrK9Xo8D6F_l0Q8cN">CONTÁCTAME</button></a>
+	</div>
+
+</div>
+</body>
+
+<br>
+
+<footer>
+	<div class="container" align="center">
+		<h4>¿DÓNDE RADICO?</h4>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d943.5542382777169!2d-99.23449927079898!3d18.921788799198566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTjCsDU1JzE4LjQiTiA5OcKwMTQnMDIuMiJX!5e0!3m2!1ses-419!2smx!4v1599721421734!5m2!1ses-419!2smx" width="1000" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+	</div>
+
+<br>
+
+</footer>
+</html>
